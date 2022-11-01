efa144ddd5bde8e8e629d144dbb9c392e6bc6083
gmvkrishna <venkatkrishnagoli@gmail.com>
2022-11-01 19:08:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+
+class StarWars extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      loadedCharacter: false,
+      name: null,
+      image: null,
+      height: null,
+      homeworld: null,
+    }
+  }
+ 
+  getNewCharacter() {
+    const randomNumber = Math.round(Math.random() * 88 )
+    const url = `https://akabab.github.io/starwars-api/api/id/${randomNumber}.json`
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+    this.setState ({
+      image: data.image,
+      name: data.name,
+      height: data.height,
+      homeworld: data.homeworld,
+      loadedCharacter: true,
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.loadedCharacter &&
+        <div>
+          <img src={this.state.image} alt="" id="img"/>
+          <h1>{this.state.name}</h1>
+          
+          <p>Height: {this.state.height} cm</p>
+          <p>HomeWorld: {this.state.homeworld}</p>
+        </div>
+        }   
+        <button
+          type="button"
+          onClick={() => this.getNewCharacter()}
+          className="btn"
+        >
+          Generate character
+        </button>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StarWars />
       </header>
     </div>
   );
